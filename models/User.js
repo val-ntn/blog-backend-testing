8f07fb9da84bf96e1384577ff182f1580051dc3d
val-ntn <valw108@gmail.com>
2025-05-23 13:52:20 +0200

    Set up admin creation

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 //models/User.js
 
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 // Define the User schema structure
 const userSchema = new mongoose.Schema({
@@ -17,11 +18,29 @@ const userSchema = new mongoose.Schema({
     type: String,       // User role with restricted values, default is 'author'
     enum: ['admin', 'author', 'subscriber'],
     default: 'author'
-  }
+  },
+  password: { type: String, required: true }
+
 }, { 
   timestamps: true      // Automatically add createdAt and updatedAt timestamps
 });
+// Pre-save hook to hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next(); // only hash if password changed or new
 
+  try {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Method to compare password during login
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 // Export the User model based on userSchema
 export default mongoose.model('User', userSchema);
 
